88ff3b9e621df7554d33e839e1e888766c7efe4f
Kekeking <erikmreyesm@gmail.com>
2026-08-13 17:58:04 -0600

    feat: update AmounDisplay separator, ExpenseDetails icons, and date formatter
    - Added separator in AmounDisplay to prevent ':' from sticking to expenses
    - Implemented icons and variable in ExpenseDetails to iterate over objects like a database
    - Added helper function in index to format dates in week/day/year style (US format)

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,11 +1,37 @@
 import type { Expense } from "../types"
+import { formatDate } from "../helpers/index"
+import AmountDisplay from "./AmountDisplay"
+import { useMemo } from "react"
+import { categories } from "../data/categories"
 
 type ExpenseDetailProps = {
     expense: Expense
 }
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+
+  const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category[0]), [expense])
+
   return (
-    <div>ExpenseDetail</div>
+    <div className="flex gap-5 items-center bg-white shadow-lg p-10 w-full border-b border-gray-200">
+      <div>
+        <img 
+          src={`/icono_${categoryInfo[0]?.icon}.svg`}
+          alt="Expense Icon"
+          className="w-20"
+        />
+      </div>
+      
+      <div className="flex-1 space-y-2">
+        <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo[0]?.name}</p>
+        <p>{expense.expenseName}</p>
+        {/* The ! after expense.date indicates that it's a non-null assertion, meaning we're telling TypeScript that we know the value is not null or undefined */}
+        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+      </div>
+      
+      <AmountDisplay
+        amount={expense.amount}
+      />
+    </div>
   )
 }
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,16 @@
 export function formatCurrency(amount: number) {
     return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(amount)
 }
+
+//Returns a formatted date string in the format "Weekday, Month Day, Year"
+export function formatDate(dateStr: string) : string {
+    const dateObj = new Date(dateStr)
+    const options : Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+
+    return new Intl.DateTimeFormat('en-US', options).format(dateObj)
+}
